4e02c0c571cd79070b3207af465ecc3c8ed8406f
tobias.kuegerl <tobikg2201@gmail.com>
2024-10-23 10:14:05 +0200

    ++

---
+++ main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from selenium.webdriver import Edge
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.edge.options import Options
+from fastapi import FastAPI
+
+PORT = 5050
+HOST = "localhost"
+
+app = FastAPI()
+
+@app.get("/temperature")
+def get_temperature():
+    chrome_options = Options()
+    wd = Edge(options=chrome_options)
+    wd.get("https://www.bergfex.at/sommer/eibiswald/wetter/")
+    return wd.find_element(By.CLASS_NAME, 'value').text
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host=HOST, port=PORT)
