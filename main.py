fb951f12dbdb15fabb82e13b114e5a72b7c2ecb9
tobias.kuegerl <tobikg2201@gmail.com>
2024-10-29 09:03:15 +0100

    ++ init

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import uvicorn
-from selenium.webdriver import Edge
-from selenium.webdriver.common.by import By
+import subprocess
+import os
 
-from selenium.webdriver.edge.options import Options
-from fastapi import FastAPI
-
-PORT = 8080
-HOST = "weather-production-94ed.up.railway.app"
-
-app = FastAPI()
-
-@app.get("/temperature")
-def get_temperature():
-    chrome_options = Options()
-    wd = Edge(options=chrome_options)
-    wd.get("https://www.bergfex.at/sommer/eibiswald/wetter/")
-    return wd.find_element(By.CLASS_NAME, 'value').text
-
+def merge_pdfs(input_files, output_file):
+    gs_command = [
+        r'C:\Program Files\gs\gs10.04.0\bin\gswin64c.exe',
+        '-dBATCH',
+        '-dNOPAUSE',
+        '-sDEVICE=pdfwrite',
+        '-sOutputFile=' + output_file,
+    ] + input_files
+    try:
+        subprocess.run(gs_command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Fehler beim Zusammenführen der PDFs: {e}')
 
 if __name__ == '__main__':
-    uvicorn.run(app, host=HOST, port=PORT)
+    pdfs_to_merge = [f"temps/temp_report_{i}.pdf" for i in range(24)]
+    output_pdf = 'merged_output.pdf'
+    pdfs_to_merge = [pdf for pdf in pdfs_to_merge if os.path.isfile(pdf)]
+    if not pdfs_to_merge:
+        print('Keine gültigen PDF-Dateien gefunden.')
+    else:
+        merge_pdfs(pdfs_to_merge, output_pdf)
